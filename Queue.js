0b5de4d2f9b0c4f27a52ddcd0c11ac1cbe76eb23
Ali_Asad <ali.asad01@yahoo.com>
2023-12-24 05:58:48 +0500

    queue with objects

--- Queue.js
+++ Queue.js
@@ -1,4 +1,8 @@
-class Queue {
+// in this dequeue operation in linear time complexity
+
+console.log("queue implementation using array");
+
+class QueueArray {
   constructor() {
     this.list = [];
   }
@@ -28,7 +32,7 @@ class Queue {
   }
 }
 
-let queue = new Queue();
+let queue = new QueueArray();
 
 queue.enqueue(1);
 queue.enqueue(2);
@@ -47,3 +51,61 @@ queue.print();
 console.log(queue.size());
 
 console.log(queue.isEmpty());
+
+// in this enqueue, dequeue operation are of constant time complexity
+
+console.log("queue implementation using object");
+
+class QueueObject {
+  constructor() {
+    this.object = {};
+    this.rear = 0;
+    this.front = 0;
+  }
+
+  enqueue(value) {
+    this.object[this.rear] = value;
+    this.rear++;
+  }
+
+  dequeue() {
+    delete this.object[this.front];
+    this.front++;
+  }
+
+  peek() {
+    return this.object[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  isEmpty() {
+    return this.rear - this.front > 0;
+  }
+
+  print() {
+    console.log(this.object);
+  }
+}
+
+let queue2 = new QueueObject();
+
+queue2.enqueue(1);
+queue2.enqueue(2);
+queue2.enqueue(3);
+queue2.enqueue(4);
+
+queue2.print();
+
+console.log(queue2.peek());
+
+queue2.dequeue();
+queue2.dequeue();
+
+queue2.print();
+
+console.log(queue2.size());
+
+console.log(queue2.isEmpty());
